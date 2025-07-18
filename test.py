2ca8025ec5780a54a6b8c5a06b670645e93be717
Antsa-del <antsamarie.rasoanantenaina@student.passerellesnumeriques.org>
2025-07-18 17:26:00 +0300

    ajouter convert_balance

--- test.py
+++ test.py
@@ -14,3 +14,7 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def convert_balance():
+    obj = Wallet(50)
+    assert obj.convert_balance(5000)==25000
